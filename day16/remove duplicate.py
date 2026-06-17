abf7525ad29bd338b900ec72c1878e970f884f7a
manyaaa8 <manyanegi67@gmail.com>
2026-06-17 15:51:52 +0530

    Updated remove duplicate program

--- day16/remove duplicate.py
+++ day16/remove duplicate.py
@@ -1,9 +1,12 @@
 from array import *
-arr=array("i",[])
-n=int(input("enter the number of elements to be inserted:-"))
+arr = array("i",[])
+n = int(input("enter the number of elements to be inserted:-"))
 for i in range(n):
-    x=int(input("ENTER THE ELEMENTS:-"))
+    x = int(input("ENTER THE ELEMENTS:-"))
     arr.append(x)
-new=set(arr)
-for i in new:
- print(i,end=" ")
+newarr = array("i",[])
+for i in arr:
+    if i not in newarr: #use membership operator to check whether i is not present in newarr if not then 
+        newarr.append(i) #append the element
+arr=newarr
+print(arr)
