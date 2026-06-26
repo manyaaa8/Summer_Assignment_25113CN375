e68d026f02b7f2a32803f496bd8e320b5d242ae2
manyaaa8 <manyanegi67@gmail.com>
2026-06-26 16:29:21 +0530

    Add Day 24 string programs

--- day24/checkstrot.py
+++ day24/checkstrot.py
@@ -1,7 +1,8 @@
-st=input("enter the string:-")
-word=st.split(" ")
-longest=word[0]
-for i in word:
-    if len(i)>len(longest):
-        longest=i
-print("the longest word is",longest)
+st1 = input("ENTER THE FIRST STRING: ")
+st2 = input("ENTER THE SECOND STRING: ")
+if len(st1) != len(st2):
+    print("NOT A ROTATION")
+elif st2 in (st1 + st1):
+    print("ROTATION")
+else:
+    print("NOT A ROTATION")
